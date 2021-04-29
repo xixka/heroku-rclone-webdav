782948b58dc29573d31a5fa756f4e20e3ecf6236
xixka <xka@live.com>
2021-04-29 15:01:55 +0800

    修改说明

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ cat << EOF > /.config/rclone/rclone.conf
 $rclone
 EOF
 # 启动rclone
-exec /usr/bin/rclone serve webdav ${name} --buffer-size 32M --vfs-cache-mode writes --addr :${PORT}  --user ${user} --pass ${pass}
+exec /usr/bin/rclone serve webdav ${path} --buffer-size 32M --vfs-cache-mode writes --addr :${PORT}  --user ${user} --pass ${pass}
